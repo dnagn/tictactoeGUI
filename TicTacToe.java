ed6f6cf38ee7056e7ee69c6aeb63896eddd310a6
dnagn <ngandang721@gmail.com>
2025-07-29 18:45:31 -0400

    create a method for setting up board

--- TicTacToe.java
+++ TicTacToe.java
@@ -15,21 +15,31 @@ public class TicTacToe {
         width = this.width;
         height = this.height;
         button = new JButton[3][3];
-
-        // set up the board
-        for (int row = 0; row < button.length; row++) {
+        /* for (int row = 0; row < button.length; row++) {
             for (int col = 0; col < button[0].length; col++) {
                 button[row][col] = new JButton("");
                 frame.add(button[row][col]);
             }
-        }
+        } */
     }
 
     public void setUpTicTacToe() {
         frame.setLayout(new GridLayout(3, 3));
         frame.setSize(width, height);
+        setUpBoard(button);
         frame.setTitle("Tic Tac Toe");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
+
+    private void setUpBoard(JButton[][] board) {
+        for (int row = 0; row < button.length; row++) {
+            for (int col = 0; col < button[0].length; col++) {
+                board[row][col] = new JButton("");
+                frame.add(board[row][col]);
+            }
+        }
+    }
+
+
 }
